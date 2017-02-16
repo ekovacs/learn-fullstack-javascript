9e3620a8d0e7a9523ef6c203f5227792aae8950d
Endre Kovacs <ekovacs@b2international.com>
2017-02-16 15:56:18 +0100

    using js variables inside JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const color = Math.random() > 0.5 ? 'green' : 'red'
+
+/* color: color => can be shorthanded as color, since the property name and its variable value name matches*/
 ReactDOM.render(
-    React.createElement("h2", null, "Hello, rendered by React!"),
+    <h2 style={{color}}>Hello, rendered by React! Powered by JSX! -- {Math.random()}</h2>,
     document.getElementById("root")
 );
