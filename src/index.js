b533085c763ef5b1e97a03c24f5abc76bc0826d5
Endre Kovacs <ekovacs@b2international.com>
2017-02-16 18:28:03 +0100

    added default prop value and some comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const App = (props) => {
     return (
         <h2 className="text-center">
@@ -9,12 +8,19 @@ const App = (props) => {
                 {props.headerMessage}
         </h2>
     );
-
 };
 
+
+// adding the .isRequired makes sure the value is required
 App.propTypes = {
-    headerMessage: React.PropTypes.string
+    headerMessage: React.PropTypes.string.isRequired
 };
+
+
+// Default value for props can be defined like this:
+App.defaultProps = {
+    headerMessage: 'Hello!'
+}
 
 ReactDOM.render(
     <App headerMessage="rendered by a react component!" />,
