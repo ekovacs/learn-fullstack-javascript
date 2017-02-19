0e6fb26f8812ff6c377c5b670c15789f81840b67
Endre Kovacs <ekovacs@b2international.com>
2017-02-19 18:29:56 +0100

    using node-sass-middleware to compile sass
    hooking in compiled css in header EJS

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ import config from './config';
 import express from 'express';
 import fs from 'fs';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+
 
 const server = express();
 
 server.set('view engine', 'ejs');
 
+server.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')
+}));
 
 server.get("/", (req, resp) => {
     resp.render('index', {
