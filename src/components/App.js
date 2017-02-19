bb96e7f85a65c420877fee561b6aebe473227812
Endre Kovacs <ekovacs@b2international.com>
2017-02-19 16:58:58 +0100

    App made statefull react component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 
-const App = () => {
-    return (
-        <div className="App">
-           <Header message="Naming Contests"/>
-           <Content />
-        </div>
-    );
+class App extends React.Component  {
+
+    state = { 
+        pageHeader: 'Naming contest'
+    };
+    
+
+    
+    render () {
+        return (
+            <div className="App">
+               <Header message={this.state.pageHeader} />
+               <Content />
+            </div>
+        );
+    };
+
+    
 };
 
 export default App;
