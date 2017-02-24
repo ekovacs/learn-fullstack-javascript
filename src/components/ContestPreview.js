4b8f020091520d5f189f30f46564f4fd387ef8a5
Endre Kovacs <ekovacs@b2international.com>
2017-02-24 11:17:52 +0100

    made ContestPreview stateful added onClick funct.

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const ContestPreview = (contest) => {
-  return (
-        <div className="ContestPreview"> 
-            <div className="category-name">{contest.categoryName}</div>
-            <div className="contest-name">{contest.contestName}</div>
+
+
+class ContestPreview extends React.Component {
+
+  handleClick = () => {
+    console.log(this.props.contestName);
+  };
+
+  render() {
+    return (
+        <div className="ContestPreview" onClick={this.handleClick}> 
+            <div className="category-name">{this.props.categoryName}</div>
+            <div className="contest-name">{this.props.contestName}</div>
         </div>
-  );
+    );
+  }
+
+}
+
+// Prop validations
+ContestPreview.propTypes = {
+  categoryName: React.PropTypes.string.isRequired,
+  contestName: React.PropTypes.string.isRequired
 };
-
-
 
 export default ContestPreview;
 
